88f664a0ccb77b67f60a90ae83786721e59693ca
Joseph Santiago Portilla <37479719+JoePortilla@users.noreply.github.com>
2024-01-22 14:44:02 -0500

    zero

---
+++ src/test/java/co/joeportilla/springoauth2jwt/SpringOauth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package co.joeportilla.springoauth2jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
